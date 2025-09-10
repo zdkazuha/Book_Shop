5bcb1b661876d127e42502577fc6fcb4f936637b
zdkazuha <aartemchelyk@gmail.com>
2025-09-10 17:54:32 +0300

    Create Config-Modal-Partial

---
+++ BookShop/Models/ConfirmDialogViewModel.cs
@@ -0,0 +1,18 @@
+namespace BookShop.Models
+{
+    public enum DialogType
+    {
+        danger,
+        info,
+        success,
+        warning
+    }
+
+    public class ConfirmDialogViewModel
+    {
+        public string DialogId { get; set; }
+        public string? Title { get; set; }
+        public string? Message { get; set; }
+        public DialogType Mode { get; set; } = DialogType.info;
+    }
+}
--- BookShop/wwwroot/js/site.js
+++ BookShop/wwwroot/js/site.js
@@ -2,3 +2,16 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+function configConfirmModal(dialogId, confirmUrl)
+{
+    var modal = document.getElementById(dialogId)
+    var deleteBtn = modal.querySelector('.actionBtn')
+
+    modal.addEventListener('show.bs.modal', function (event) {
+        var button = event.relatedTarget
+
+        var id = button.getAttribute('data-bs-id')
+        deleteBtn.setAttribute('href', confirmUrl + id)
+    })
+}
